b8ef5778560b2be2ce6067b90b018fa2b71ad421
asoliman92 <abdelrahman.ihab.soliman@gmail.com>
2023-09-03 18:12:46 +0400

    Adding proof of work

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,30 +1,25 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
+type BlockChain struct {
+	Blocks []*Block
+}
 
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
 
-type BlockChain struct {
-	Blocks []*Block
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
 }
 
 func (chain *BlockChain) AddBlock(data string) {
